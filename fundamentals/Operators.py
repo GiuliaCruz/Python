d99f722bbd08bba27b9e4a29cb1a159233cf0fe0
GiuliaCruz <giuliacruzz@gmail.com>
2024-03-14 17:03:06 -0300

    commit

--- fundamentals/Operators.py
+++ fundamentals/Operators.py
@@ -10,7 +10,6 @@ print(10 % 3)
 #challenge
 wage = 3450.45
 expenses = 2456.2
-
 percentage_committe = expenses / wage * 100 #answer
 print(percentage_committe)
 
@@ -19,3 +18,33 @@ print(3 > 4)
 print(4 >= 3)
 print(1 < 2)
 print(3 <= 1)
+print(3 != 2) #difference
+print(3 == 3) #equal
+print(2 == '2')
+
+#assignment operators
+a = 3
+a = a + 7
+print(a)
+
+a += 5 
+print(a)
+
+a -= 3
+print(a)
+
+a *= 2
+print(a)
+
+a/= 4
+print(a)
+
+a %= 4
+print(a)
+
+a**= 8
+print(a)
+
+a //= 256
+print(a)
+
