89ebf30080a8a7b45f5a771fa9d9766c37f0f48e
GiuliaCruz <giuliacruzz@gmail.com>
2024-03-14 16:36:24 -0300

    first commit

---
+++ fundamentals/Basic.py
@@ -0,0 +1,18 @@
+nome = 'exemplo'
+print(nome)
+
+print('You are ' + 3 * 'very ' + 'nice!' )
+print([1, 2, 3]) #list
+print({'name': 'Pedro', 'age' : 22 }) #dictionary
+print(None)
+print(True) #boolean
+print(False) #boolean
+print(1.2 + 1) #float
+
+a = 10
+b = 5.2
+
+print(a + b)
+a = 'Hello'
+print(a)
+
---
+++ fundamentals/Operators.py
@@ -0,0 +1,21 @@
+#arithmetic operators
+print(2 + 3)
+print(4 - 7)
+print(2 * 5.3)
+print(9.4 / 3)
+print(9.4 // 3) #must be an integer float
+print(2 ** 8)
+print(10 % 3)
+
+#challenge
+wage = 3450.45
+expenses = 2456.2
+
+percentage_committe = expenses / wage * 100 #answer
+print(percentage_committe)
+
+#relational operators
+print(3 > 4)
+print(4 >= 3)
+print(1 < 2)
+print(3 <= 1)
